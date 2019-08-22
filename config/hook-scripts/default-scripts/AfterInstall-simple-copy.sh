8711d92aa691f2ae99cebc60d6b866e03b92e6a4
Matteo Riva <muradin@gmail.com>
2019-08-22 16:33:15 +0200

    Improve wrapper and default copy scripts

--- config/hook-scripts/default-scripts/AfterInstall-simple-copy.sh
+++ config/hook-scripts/default-scripts/AfterInstall-simple-copy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+set -xe
+
 # Build archive path from AWS Codedeploy environment variables
 # The source is the temporary folder where Codedeploy stores the uncompressed archive of the current revision
 ARCHIVE_DIR="/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive"
 
-# Add real destination directory and uncomment next line
-#rsync -a "${ARCHIVE_DIR}" DESTINATION_DIRECTORY
+# Add real destination directory
+TARGET_DIR=
+
+if [ -z $TARGET_DIR ];
+then
+    echo "Target directory not defined."
+    exit 1
+fi
+
+rsync -a --exclude-from="${ARCHIVE_DIR}/deploy/hook-scripts/rsync_exclude" "${ARCHIVE_DIR}/" "${TARGET_DIR}"
--- config/hook-scripts/hook-wrapper.sh
+++ config/hook-scripts/hook-wrapper.sh
@@ -4,8 +4,12 @@ AGENT_ROOT=/opt/codedeploy-agent
 
 COMMAND="${AGENT_ROOT}/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive/deploy/hook-scripts/${DEPLOYMENT_GROUP_NAME}/${LIFECYCLE_EVENT}.sh"
 
+LOG_LABEL="[${APPLICATION_NAME}:${DEPLOYMENT_GROUP_NAME}:${LIFECYCLE_EVENT}]"
+
 if [[ -f $COMMAND ]]; then
-    echo "[${APPLICATION_NAME}:${DEPLOYMENT_GROUP_NAME}:${LIFECYCLE_EVENT}] Execution started" | ts '[%Y-%m-%d %H:%M:%S%z]' >> ${HOME}/logs/deploy.log
-    source $COMMAND | ts '[%Y-%m-%d %H:%M:%S%z]' >> ${HOME}/logs/deploy.log 2>&1
-    echo "[${APPLICATION_NAME}:${DEPLOYMENT_GROUP_NAME}:${LIFECYCLE_EVENT}] Execution ended" | ts '[%Y-%m-%d %H:%M:%S%z]' >> ${HOME}/logs/deploy.log
+    {
+      echo "Execution started" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
+      source "$COMMAND" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
+      echo "Execution ended" | ts "[%Y-%m-%d %H:%M:%S%z] ${LOG_LABEL}"
+    } >> "${HOME}/logs/deploy.log" 2>&1
 fi
